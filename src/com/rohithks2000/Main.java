d56f2b727c264d1839b600c1a8e7669d6fab1960
Rohith <rohithks2000@gmail.com>
2022-04-03 18:59:57 -0400

    types final

--- src/com/rohithks2000/Main.java
+++ src/com/rohithks2000/Main.java
@@ -1,9 +1,19 @@
 package com.rohithks2000;
 
+import java.math.BigInteger;
+import java.text.Format;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("Mortgage Calculator");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal : ");
+        BigInteger principal = scanner.nextBigInteger();
+
+
     }
 }
